1d3624069640fa6112ad6483e0ebf84623900b63
williew <williew3@protonmail.com>
2024-07-16 21:54:21 -0400

    test

--- src/controllers/wakeWordController.js
+++ src/controllers/wakeWordController.js
@@ -106,7 +106,7 @@ const initializeWakeWordDetection = async () => {
             logWithTimestamp(`Stack trace: ${error.stack}`);
         }
     }
-};
+}; 
 
 module.exports = {
     initializeWakeWordDetection,
